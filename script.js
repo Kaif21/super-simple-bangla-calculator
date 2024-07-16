a6523f64a88be3499f4daf3478e19bb0c8f8e6bb
kaif21 <kaif.bd21@gmail.com>
2024-07-16 16:39:36 +0600

    added bengali number symbol in the numpad

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 let inputBox = "";
+let convertedMath = "";
 let keys = document.querySelectorAll('.key');
+// just change the symbol to change the language
+const symbolMap = {
+    '০': '0', '১': '1', '২': '2', '৩': '3', '৪': '4',
+    '৫': '5', '৬': '6', '৭': '7', '৮': '8', '৯': '9',
+    '÷': '/', '*':'X'
+};
+
+const reverseSymbolMap = Object.fromEntries(
+    Object.entries(symbolMap).map(([key, value]) => [value, key])
+);
+
+function convertToSymbol(str) {
+    return str.split('').map(char => reverseSymbolMap[char] || char).join('');
+}
+
+function convertToNumber(str) {
+    return str.split('').map(char => symbolMap[char] || char).join('');
+}
 
 Array.from(keys).forEach((key) => {
     key.addEventListener('click', (e) => {
-        if (e.target.innerHTML == '=') {
+        let value = e.target.dataset.value || e.target.innerHTML;
+        let display = e.target.innerHTML;
+
+        if (display == '=') {
             if (inputBox.includes('%')) {
                 let [number, percentage] = inputBox.split('*');
                 inputBox = (parseFloat(number) * parseFloat(percentage) / 100).toString();
             } else {
                 inputBox = eval(inputBox).toString();
             }
-        } else if (e.target.innerHTML == 'C') {
+            convertedMath = convertToSymbol(inputBox);
+        } else if (display == 'C') {
             inputBox = "";
+            convertedMath = "";
         } else {
-            inputBox += e.target.innerHTML;
+            inputBox += value;
+            convertedMath += display;
         }
-        document.querySelector('input').value = inputBox;
+        document.querySelector('input').value = convertedMath;
     });
 });
-
-// make an else for % calculation
-// make an btn to delete only 1 key in the inputbox
-//empty the inputbox after result when type some thing else
-//make * look like X
-  
